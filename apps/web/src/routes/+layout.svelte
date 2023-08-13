88c8c023db8657be14a2f805245cfc8e07e8e91f
Jesus Cova <jesuscovam@gmail.com>
2023-08-13 08:26:56 -0600

    transicion entre cambios de ruta

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
 	import '../app.css';
+	import { fade } from 'svelte/transition';
+	export let data;
 </script>
 
-<slot />
+{#key data.url}
+	<div in:fade={{ duration: 300, delay: 400 }} out:fade={{ duration: 300 }}>
+		<slot />
+	</div>
+{/key}
