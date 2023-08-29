4d412f1e9b1b2ddbd4c2341f684261b1432e9d1c
Jesus Cova <jesuscovam@gmail.com>
2023-08-28 18:19:39 -0600

    update template

--- apps/dashboard/vite.config.ts
+++ apps/dashboard/vite.config.ts
@@ -1,9 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { searchForWorkspaceRoot } from 'vite';
 import svg from '@poppanator/sveltekit-svg';
 
 export default defineConfig({
 	plugins: [sveltekit(), svg()],
+	server: {
+		host: true,
+		fs: {
+			allow: [searchForWorkspaceRoot(process.cwd()), '/packages/assets']
+		}
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
